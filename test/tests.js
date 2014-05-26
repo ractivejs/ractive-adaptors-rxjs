c71e1f854880bdcfac38a3b2d09146031d115720
Rich Harris <richard.a.harris@gmail.com>
2014-05-25 23:22:35 -0400

    initial commit

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+// ractive-adaptors-rxjs tests
+// =====================
+
+(function () {
+
+	var fixture;
+
+	test( 'We are ready to write some tests', function ( t ) {
+		t.ok( true );
+	});
+
+
+}());
